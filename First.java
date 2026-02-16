12c681bada1308261610b51e13c8a31fc5212c78
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-16 15:13:34 +0530

    Add files via upload
     Office Salary Processing System

---
+++ First.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class First {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(num);
+    }
+}
