026ba85019f90366f5b0c6fd4bc8d712d7b0b2ff
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-17 09:17:21 +0530

    Add files via upload
     Sports Tournament Prize Fund

---
+++ Twentefour.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Twentefour {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int weight=sc.nextInt();
+        int rate=sc.nextInt();
+        int pri=sc.nextInt();
+        int insurance=sc.nextInt();
+        int hand=sc.nextInt();
+        int fuel=sc.nextInt();
+        int num1=weight*rate;
+        int num2=num1+pri+insurance-hand-fuel;
+        System.out.println(num2); 
+    }
+}
