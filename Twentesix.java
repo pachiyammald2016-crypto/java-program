20c72b291774f2f1f003f1425b1739d84bcd15a1
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-17 09:18:23 +0530

    Add files via upload

---
+++ Twentesix.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Twentesix {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int res=sc.nextInt();
+        int rate=sc.nextInt();
+        int com=sc.nextInt();
+        int c=sc.nextInt();
+        int con=sc.nextInt();
+        int pen=sc.nextInt();
+        int main=sc.nextInt();
+        int treat=sc.nextInt();
+        int num1=res*rate;
+        int num2=num1+(com*c);
+        int num3=num2+con+pen;
+        int num4=num3-main;
+        int num5=num4-treat;
+        System.out.println(num5);
+    }
+}
