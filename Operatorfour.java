126af704a3cab58c7bccefddc006b759d82503f2
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-17 09:21:30 +0530

    Add files via upload
    Credit Card Purchase Validation

---
+++ Operatorfour.java
@@ -0,0 +1,17 @@
+package Operators;
+import java.util.Scanner;
+public class Operatorfour {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int credit=sc.nextInt();
+        int purchase=sc.nextInt();
+        int block=sc.nextInt();
+        if(purchase<=credit&&block==0){
+            System.out.println("Approved");
+        }
+        else{
+            System.out.println("Declined");
+        }
+    }
+    
+}
