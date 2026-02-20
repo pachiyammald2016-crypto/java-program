bdc2a4b6d67ae330e023c0174ec4ed2a601f28d8
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-20 16:08:17 +0530

    Add files via upload
     Credit Card Reward Points Calculator

--- Seventeen.java
+++ Seventeen.java
@@ -1,18 +1,83 @@
+package Ifelse;
 import java.util.Scanner;
 public class Seventeen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int genpass=sc.nextInt();
-        int genfare=sc.nextInt();
-        int acpass=sc.nextInt();
-        int acfare=sc.nextInt();
-        int platform=sc.nextInt();
-        int maintanance=sc.nextInt();
-        int fuel=sc.nextInt();
-        int num1=genpass*genfare;
-        int num2=num1+(acpass*acfare);
-        int num3=num2+platform;
-        int num4=num3-maintanance-fuel;
-        System.out.println(num4);
+        String cardtier=sc.next();
+        String purchase=sc.next();
+        double transaction=sc.nextDouble();
+        String status=sc.next();
+
+        double basepoint=1.0;
+        double bonus=0.0;
+        if(purchase.equals("groceries")){
+            bonus=2.0;
+        }
+        if(purchase.equals("dining")){
+            bonus=2.5;
+        }
+        if(purchase.equals("travel")){
+            bonus=3.0;
+        }
+        if(purchase.equals("gas")){
+            bonus=2.0;
+        }
+        if(purchase.equals("other")){
+            bonus=1.0;
+        }
+
+        double multiplier=0.0;
+        if(cardtier.equals("basic")){
+            multiplier=1.0;
+        }
+        if(cardtier.equals("gold")){
+            multiplier=1.25;
+        }
+        if(cardtier.equals("platinum")){
+            multiplier=1.5;
+        }
+        if(cardtier.equals("black")){
+            multiplier=2.0;
+        }
+
+        double promotion=0.0;
+        if(status.equals("none")){
+            promotion=1.0;
+        }
+        if(status.equals("double-points")){
+            promotion=2.0;
+        }
+        if(status.equals("triple-points")){
+            promotion=3.0;
+        }
+
+        double pointsearned=transaction*basepoint*bonus*multiplier*promotion;
+        double cashvalue=pointsearned*0.01;
+
+        String rewards="none";
+        if(cardtier.equals("basic")){
+            rewards="standard";
+        }
+        if(cardtier.equals("gold")){
+            rewards="enhanced";
+        }
+        if(cardtier.equals("platinum")){
+            rewards="premium";
+        }
+        if(cardtier.equals("black")){
+            rewards="elite";
+        }
+
+        System.out.println("card tier: "+cardtier);
+        System.out.println("purchase category: "+purchase);
+        System.out.println("transaction amount: "+"$"+transaction);
+        System.out.println("promotional status: "+status);
+        System.out.println("base points rate: "+basepoint+" points per dollar");
+        System.out.println("category bonus: "+bonus+"x");
+        System.out.println("tier multiplier: "+multiplier+"x");
+        System.out.println("promotional multiplier: "+promotion+"x");
+        System.out.println("points earned: "+pointsearned);
+        System.out.println("equivalent cash value: "+"$"+cashvalue);
+        System.out.println("rewards tier: "+rewards);
     }
 }
