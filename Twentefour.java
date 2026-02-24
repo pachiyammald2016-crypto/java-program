fce0e3b3b41b0ac99dbe445d4c6d84cb62b99e5c
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-02-24 22:40:34 +0530

    Update Twentefour.java

--- Twentefour.java
+++ Twentefour.java
@@ -10,6 +10,7 @@ public class Twentefour {
         int fuel=sc.nextInt();
         int num1=weight*rate;
         int num2=num1+pri+insurance-hand-fuel;
-        System.out.println(num2); 
+        System.out.println(num2);
     }
 }
+
