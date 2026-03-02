b6d11022ceec88bdf92607723efd42aa1b99a9d0
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-02 20:32:33 +0530

    Add files via upload
    ATM Cash Withdrawal Validator

--- Six.java
+++ Six.java
@@ -1,72 +1,44 @@
-package Ifelse;
+package For;
 import java.util.Scanner;
 public class Six {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String book=sc.next();
-        int days=sc.nextInt();
-        double seat=sc.nextDouble();
-        String root=sc.next();
-        double amount=0.0;
-        double mul=0.0;
-        String num="none";
-        if(book.equals("economy")){
-            if(root.equals("domestic")){
-                amount=200.0;
-            }
-            else if(root.equals("in-short")){
-                amount=500.0;
-            }
-            else if(root.equals("in-lng")){
-                amount=800.0;
-            }
-        }
-        else if(book.equals("business")){
-            if(root.equals("domestic")){
-                amount=600.0;
-            }
-            else if(root.equals("in-short")){
-                amount=1500.0;
-            }
-            else if(root.equals("in-lng")){
-                amount=2500.0;
-            }
-        }
-        else if(book.equals("first")){
-            if(root.equals("domestic")){
-                amount=1000.0;
-            }
-            else if(root.equals("in-short")){
-                amount=3000.0;
-            }
-            else if(root.equals("in-lng")){
-                amount=5000.0;
-            }
-        }
-        if(seat>30&&days<14){
-            mul=1.8;
-            num="high demand";
-        }
-        else if(seat<50&&days<30){
-            mul=1.5;
-            num="high demand";
-        }
-        else if(seat<60||days>=30&&days<=60){
-            mul=1.0;
-            num="moderate";
-        }
-        else if(seat>=60&&days>60){
-            mul=0.8;
-            num="low demand";
-        }
-        double total=amount*mul;
-        System.out.println("booking class: "+book);
-        System.out.println("days: "+days);
-        System.out.println("seat available: "+seat+"%");
-        System.out.println("route type: "+root);
-        System.out.println("base price: "+"$"+amount);
-        System.out.println("demand: "+mul+"x");
-        System.out.println("final price: "+"$"+total);
-        System.out.println("category: "+num);
-    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double initial=sc.nextDouble();
+		int n=sc.nextInt();
+		double sum=0.0;
+		int count=0;
+		int count1=0;
+		String status="";
+		;double add=0.0;
+		double amount=0.0;
+		for(int i=1;i<=n;i++) {
+			double withdraw=sc.nextDouble();
+			amount=initial-withdraw;
+			if(withdraw<=amount) {
+				initial=amount;
+				status="approved";
+				sum+=amount;
+				count++;
+				
+			}
+			else {
+				status="denied";
+				count1++;
+			}
+			System.out.println("transaction"+ i+":"+"$"+withdraw);
+			add+=withdraw;
+			System.out.println("status: "+status);
+			if(status.equals("denied")) {
+				System.out.println("reason: "+"Insufficient funds");
+			}
+			System.out.println("remaining balance: "+"$"+"$"+amount);
+			System.out.println();
+		}
+		System.out.println("total transactions: "+n);
+		System.out.println("successful withdraw: "+count);
+		System.out.println("failed withdraw: "+count1);
+		System.out.println("final balance: "+"$"+amount);
+		System.out.println("total withdraw: "+"$"+add);
+		
+	}
 }
