d577e8c231a68ee46b4716915b7e4cd398cf05e7
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-03 06:19:21 +0530

    Add files via upload

--- Eighteen.java
+++ Eighteen.java
@@ -1,95 +1,56 @@
-package Ifelse;
+package For;
 import java.util.Scanner;
 public class Eighteen {
-   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    double pack=sc.nextDouble();
-    String destination=sc.next();
-    String shipping=sc.next();
-    String size=sc.next();
+    public static void main(String[] args){
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+        String status="";
+        int count1=0;
+        int count2=0;
+        double total=0.0;
+        int max=0;
+        int min=0;
+        double sum1=0.0;
+        double sum2=0.0;
+       for(int i=1;i<=n;i++){
+        int hour=sc.nextInt();
+        double temp=sc.nextDouble();
+        if(hour>max){
+            max=hour;
+            sum1=temp;
 
-    double baserate=0.0;
-    if(shipping.equals("economy")){
-        baserate=5.0;
-    }
-    else if(shipping.equals("standard")){
-        baserate=10.0;
-    }
-    else if(shipping.equals("express")){
-        baserate=25;
-    }
-    else if(shipping.equals("overnight")){
-        baserate=50;
-    }
+        }
+        if(hour<min){
+            min=hour;
+            sum2=temp;
+        }
+        if(temp>40||temp<0){
+            status="extreme";
+        }
+        else if(temp>=32&&temp<=40){
+            status="hot";
+            count1++;
+        }
+        else if(temp>=0&&temp<=10){
+            status="cold";
+            count2++;
+        }
+        else if(temp>=10&&temp<=32){
+            status="normal";
+        }
 
-    double weight=pack*2;
-    if(pack<15){
-        weight=pack;
-    }
-    double multiplier=0.0;
-    if(destination.equals("local")){
-        multiplier=1.0;
-    }
-     else if(destination.equals("regional")){
-        multiplier=1.2;
-    }
-     else if(destination.equals("national")){
-        multiplier=1.5;
-    }
-     else if(destination.equals("international")){
-        multiplier=3.0;
-    }
+        total+=temp;
 
-    double add=0.0;
-    if(size.equals("small")){
-        add=0;
-    }
-    else if(size.equals("medium")){
-        add=5;
-    }
-    else if(size.equals("large")){
-        add=15;
-    }
-     else if(size.equals("oversized")){
-        add=30;
-    }
+        System.out.println("hours: "+hour);
+        System.out.println("status: "+status);
+       } 
+       double avg=total/n;
+        System.out.println("total readings: "+n);
+        System.out.println("avg temperature: "+avg);
+        System.out.println("highest temperature: "+sum1+"C"+" at hour "+max);
+        System.out.println("lowest temperature: "+sum2+"C"+" at hour "+min);
+        System.out.println("hot hours: "+count1);
+        System.out.println("cold hours: "+count2);
 
-    double basecost=(baserate+weight)*multiplier;
-    double totalcost=basecost+add;
-
-    String delivery="none";
-    int time=0;
-    if(destination.equals("local")&&shipping.equals("economy")){
-        delivery="budget";
-        time=7;
     }
-    if(destination.equals("regional")&&shipping.equals("standard")){
-        delivery="standard";
-        time=5;
-    }
-    if(destination.equals("national")&&shipping.equals("express")){
-        delivery="priority";
-        time=2;
-    }
-    if(destination.equals("international")&&shipping.equals("overnight")){
-        delivery="premium";
-        time=1;
-    }
-
-    System.out.println("package weight: "+pack+" lbs");
-    System.out.println("destination zone: "+destination);
-    System.out.println("shipping speed: "+shipping);
-    System.out.println("package size: "+size);
-    System.out.println("base shipping rate: "+"$"+baserate);
-    System.out.println("weight surcharge: "+"$"+weight);
-    System.out.println("zone multiplier: "+"$"+multiplier);
-    System.out.println("size surcharge: "+"$"+add);
-    System.out.println("total shipping cost: "+"$"+totalcost);
-    System.out.println("estimated delivery: "+time+" business days");
-    System.out.println("service level: "+delivery);
-
-
-    
-   } 
 }
-
