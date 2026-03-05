ac034bdaf0fb4c6e6a12e56d87cef5f21cb97318
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-05 19:14:10 +0530

    Add files via upload
     ATM Withdrawal Validator (DoWhile)

--- Six.java
+++ Six.java
@@ -1,44 +1,26 @@
-package For;
+package While;
 import java.util.Scanner;
 public class Six {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		double initial=sc.nextDouble();
-		int n=sc.nextInt();
-		double sum=0.0;
-		int count=0;
-		int count1=0;
-		String status="";
-		;double add=0.0;
-		double amount=0.0;
-		for(int i=1;i<=n;i++) {
-			double withdraw=sc.nextDouble();
-			amount=initial-withdraw;
-			if(withdraw<=amount) {
-				initial=amount;
-				status="approved";
-				sum+=amount;
-				count++;
-				
-			}
-			else {
-				status="denied";
-				count1++;
-			}
-			System.out.println("transaction"+ i+":"+"$"+withdraw);
-			add+=withdraw;
-			System.out.println("status: "+status);
-			if(status.equals("denied")) {
-				System.out.println("reason: "+"Insufficient funds");
-			}
-			System.out.println("remaining balance: "+"$"+"$"+amount);
-			System.out.println();
-		}
-		System.out.println("total transactions: "+n);
-		System.out.println("successful withdraw: "+count);
-		System.out.println("failed withdraw: "+count1);
-		System.out.println("final balance: "+"$"+amount);
-		System.out.println("total withdraw: "+"$"+add);
-		
-	}
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int rem=0;
+        do {
+            System.out.print("Enter amount (0 to exit): ");
+            m = sc.nextInt();
+
+            if(m == 0) {
+                System.out.println("Transaction ended.");
+            }
+            else if(m > n){
+                System.out.println("Insufficient funds! Try again.");
+            }
+            else{
+                n = n - m;
+                System.out.println("Withdrawal successful! Remaining balance: " + n);
+            }
+
+        } while(m != 0);
+    }
 }
