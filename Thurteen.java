4cfa6a44f2c83f5eb64787613d77f47b517bc245
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-05 19:19:16 +0530

    Add files via upload
     Power Calculator

--- Thurteen.java
+++ Thurteen.java
@@ -1,51 +1,16 @@
-package For;
+package While;
 import java.util.Scanner;
 public class Thurteen {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int eligibleCount = 0;
-        int rejectedCount = 0;
-        for (int i = 1; i <= n; i++) {
-            String name = sc.next();
-            int age = sc.nextInt();
-            double weight = sc.nextDouble();
-            double hemoglobin = sc.nextDouble();
-            boolean eligible = true;
-            String reason = "";
-            if (age < 18 || age > 65) {
-                eligible = false;
-                reason = "Age must be between 18 and 65 years";
-            }
-            else if (weight < 50) {
-                eligible = false;
-                reason = "Weight below 50 kg";
-            }
-            else if (hemoglobin < 12.5) {
-                eligible = false;
-                reason = "Hemoglobin below 12.5 g/dL";
-            }
-            if (eligible) {
-                eligibleCount++;
-            } else {
-                rejectedCount++;
-            }
-            System.out.println("Donor: " + name);
-            System.out.println("Age: " + age + " years");
-            System.out.println("Weight: " + weight + " kg");
-            System.out.println("Hemoglobin: " + hemoglobin + " g/dL");
-            if (eligible) {
-                System.out.println("Status: Eligible");
-            } else {
-                System.out.println("Status: Not Eligible");
-                System.out.println("Reason: " + reason);
-            }
-            System.out.println();
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        int i=1;
+        int fact=1;
+        while(i<=a){
+            fact*=n;
+            i++;
         }
-        double eligibilityRate = (eligibleCount * 100.0) / n;
-        System.out.println("Total Donors: " + n);
-        System.out.println("Eligible Donors: " + eligibleCount);
-        System.out.println("Rejected Donors: " + rejectedCount);
-        System.out.println("Eligibility Rate: " + eligibilityRate + "%");
+        System.out.println(n+"^"+a+" = "+fact);
     }
 }
