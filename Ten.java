fa2fad4596dae90d81f9f5d5022293b06f9d506c
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-05 19:16:12 +0530

    Add files via upload
     Fibonacci Sequence Generator

--- Ten.java
+++ Ten.java
@@ -1,34 +1,21 @@
-package For;
+package While;
 import java.util.Scanner;
 public class Ten {
     public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		double subtotal=0.0;
-		int total=0;
-		double value=0.0;
-		for(int i=1;i<=n;i++) {
-			String name=sc.next();
-			double price=sc.nextDouble();
-			int quantity=sc.nextInt();
-			
-			subtotal=price*quantity;
-			total+=quantity;
-			value+=subtotal;
-			System.out.println("item: "+name);
-			System.out.println("price: "+"$"+price+"x"+quantity);
-			System.out.println("subtotal: "+"$"+subtotal);
-		}
-		double tax=value*0.08;
-		double charge=value*0.10;
-		double finalvalue=value+tax+charge;
-		System.out.println("total items: "+total);
-		System.out.println("subtotal: "+value);
-		System.out.println("tax(8%): "+tax);
-		System.out.println("service charge(10%): "+charge);
-		System.out.println("grand total: "+finalvalue);
-		
-	}
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=3;
+        int a=0;
+        int b=1;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        while(i<=n){
+            int temp=a+b;
+            a=b;
+            b=temp;
+            i++;
+
+            System.out.print(temp+" ");
+        }
+    }
 }
-
-
