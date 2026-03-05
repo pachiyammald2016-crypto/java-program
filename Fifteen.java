3de31551e28d85548c17bf08c01533a57fa422b2
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-05 19:20:35 +0530

    Add files via upload
    Collatz Sequence (DoWhile)

--- Fifteen.java
+++ Fifteen.java
@@ -1,47 +1,22 @@
-package For;
+package While;
 import java.util.Scanner;
 public class Fifteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        String num="";
-        int count1=0;
-        int count2=0;
-        int highest=0;
-        int sum=0;
-        int max=0;
-        for(int i=0;i<n;i++){
-            String name=sc.next();
-            int answers=sc.nextInt();
-            int wrong=sc.nextInt();
-            int attempt=sc.nextInt();
-
-            int score=(answers*4)-(wrong*3)+(attempt*0);
-            if(score>=32){
-                num="pass";
-                count1++;
+        int org=n;
+        int count=0;
+        do{
+            if(n%2==0){
+                n=n/2;
             }
             else{
-                num="fail";
-                count2++;
+                n=3*n+1;
             }
-            if(score>max){
-                max=score;
-            }
-            sum+=score;
-            System.out.println("student: "+name);
-            System.out.println("correct: "+answers);
-            System.out.println("wrong: "+wrong);
-            System.out.println("attempt: "+attempt);
-            System.out.println("score: "+score);
-            System.out.println("result: "+num);
-
+           System.out.println(n);
+            count++;
         }
-        double avg=sum/n;
-        System.out.println("total students: "+n);
-        System.out.println("pass count: "+count1);
-        System.out.println("fail count: "+count2);
-        System.out.println("class average: "+avg);
-        System.out.println("highest score: "+max);
+        while(n!=1);
+        System.out.println("steps: "+count);
     }
 }
