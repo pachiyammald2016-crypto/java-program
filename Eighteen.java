50c200dbfc2eb7e34aca60d4b574657482080527
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-06 18:21:39 +0530

    Add files via upload
     LCM Calculator

--- Eighteen.java
+++ Eighteen.java
@@ -1,56 +1,22 @@
-package For;
+package While;
 import java.util.Scanner;
 public class Eighteen {
     public static void main(String[] args){
-       Scanner sc=new Scanner(System.in);
-       int n=sc.nextInt();
-        String status="";
-        int count1=0;
-        int count2=0;
-        double total=0.0;
-        int max=0;
-        int min=0;
-        double sum1=0.0;
-        double sum2=0.0;
-       for(int i=1;i<=n;i++){
-        int hour=sc.nextInt();
-        double temp=sc.nextDouble();
-        if(hour>max){
-            max=hour;
-            sum1=temp;
-
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int m=a;
+        int r=b;
+        int gcd=0;
+        int lcm=0;
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+            gcd=a;
         }
-        if(hour<min){
-            min=hour;
-            sum2=temp;
-        }
-        if(temp>40||temp<0){
-            status="extreme";
-        }
-        else if(temp>=32&&temp<=40){
-            status="hot";
-            count1++;
-        }
-        else if(temp>=0&&temp<=10){
-            status="cold";
-            count2++;
-        }
-        else if(temp>=10&&temp<=32){
-            status="normal";
-        }
-
-        total+=temp;
-
-        System.out.println("hours: "+hour);
-        System.out.println("status: "+status);
-       } 
-       double avg=total/n;
-        System.out.println("total readings: "+n);
-        System.out.println("avg temperature: "+avg);
-        System.out.println("highest temperature: "+sum1+"C"+" at hour "+max);
-        System.out.println("lowest temperature: "+sum2+"C"+" at hour "+min);
-        System.out.println("hot hours: "+count1);
-        System.out.println("cold hours: "+count2);
-
+        System.out.println(gcd);
+        lcm=m*r/gcd;
+        System.out.println(lcm);
     }
 }
