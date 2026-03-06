78d212776017951e15d5c7ab5fcb8fe781bc031b
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-06 18:26:12 +0530

    Add files via upload
     HCF Using Subtraction Method

---
+++ Tfour.java
@@ -0,0 +1,19 @@
+package While;
+import java.util.Scanner;
+public class Tfour {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        while(a!=b){
+            if(a>b){
+                a=a-b;
+            }
+            else{
+                b=b-a;
+            }
+        }
+        System.out.println(a);
+
+    }
+}
