7d06e12226b110558fcfed1728aca4c3d5fa57ad
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-06 18:27:52 +0530

    Add files via upload

--- Four.java
+++ Four.java
@@ -1,15 +1,29 @@
-package While;
+package Nestf;
 import java.util.Scanner;
 public class Four {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int i=1;
-        int fact=1;
-        while(i<=n){
-           fact=fact*i; 
-           i++;;
+        int m=sc.nextInt();
+        double avg=0;
+        System.out.println("grade sheet:");
+        System.out.print("Students ");
+        for(int k=1;k<=m;k++){
+            System.out.print("sub:"+k+" ");
         }
-        System.out.println("Factorial of "+n+ " = "+fact);
+        System.out.println("Average");
+        for(int i=1;i<=n;i++){
+            String name=sc.next();
+            System.out.print(name+" ");
+            int sum=0;
+            for(int j=1;j<=m;j++){
+                int mark=sc.nextInt();
+                sum+=mark;
+                System.out.print(mark+" ");
+            }
+            avg=(double)sum/m;
+            System.out.print(avg+" ");
+            System.out.println();
+        }
     }
 }
