e74203fabc80fbe68c42e387a0fa5c51987c3bf9
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-06 18:20:12 +0530

    Add files via upload
    Binary to Decimal Converter

--- Sixteen.java
+++ Sixteen.java
@@ -1,70 +1,20 @@
-package For;
+package While;
 import java.util.Scanner;
+import java.lang.Math;
 public class Sixteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        double fee=0.0;
-        double discount=0.0;
-        String num="";
-        double finalfee=0.0;
-        int count=0;
-        double sum=0.0;
-        for(int i=0;i<n;i++){
-            String name=sc.next();
-            String type=sc.next();
-            int expiry=sc.nextInt();
-
-            if(type.equals("basic")){
-                fee=50;
-            }
-            else if(type.equals("premium")){
-                fee=100;
-            } 
-            else if(type.equals("VIP")){
-                fee=200;
-            }
-
-            if(expiry>=45){
-                discount=20;
-            }
-            else if(expiry>=30&&expiry<=44){
-                discount=15;
-            }
-            else if(expiry>=15&&expiry<=29){
-                discount=10;
-            }
-            else if(expiry<15){
-                discount=0;
-            }
-
-            if(expiry<10){
-                num="urgent";
-                count++;
-            }
-            else if(expiry>10&&expiry<=30){
-                num="high";
-            }
-            else if(expiry>30){
-                num="normal";
-            }
-            finalfee=fee*(1-discount/100.0);
-            sum+=finalfee;
-            System.out.println("member: "+name);
-            System.out.println("membership: "+type);
-            System.out.println("days expiry: "+expiry);
-            System.out.println("renewal fee: "+fee);
-            System.out.println("discount: "+discount);
-            System.out.println("final fee: "+finalfee);
-            System.out.println("priority: "+num);
-
+        int result=0;
+        int i=1;
+        int power=0;
+        while(n>0){
+            int digit=n%10;
+            power=digit*i;
+            result+=power;
+            n=n/10;
+            i+=i;
         }
-        double avg=sum/n;
-         
-        System.out.println("total members: "+n);
-        System.out.println("urgent renewals: "+count);
-        System.out.println("total renewal revenue: "+sum);
-        System.out.println("average renewal fee: "+avg);
-
+        System.out.println(result);
     }
 }
