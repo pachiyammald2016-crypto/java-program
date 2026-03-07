cc8983b012e9e06aa20ecd1bb7766d22e6d07a9c
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-07 19:40:52 +0530

    Add files via upload
     Network Server Response Time Monitor

--- Ten.java
+++ Ten.java
@@ -1,21 +1,35 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Ten {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int i=3;
-        int a=0;
-        int b=1;
-        System.out.print(a+" ");
-        System.out.print(b+" ");
-        while(i<=n){
-            int temp=a+b;
-            a=b;
-            b=temp;
-            i++;
-
-            System.out.print(temp+" ");
+        int[] arr=new int[n];
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>200){
+                count++;
+            }
         }
+        double avg=(double)sum/n;
+        int num=n-count;
+        double total=((double)num/n)*100.0;
+        System.out.println("Total servers: "+n);
+        System.out.println("Fastest response: "+min+" ms");
+        System.out.println("Slowest response: "+max+" ms");
+        System.out.printf("Average response:%.2f ms\n",avg);
+        System.out.println("Slow Servers(>200ms): "+count);
+        System.out.printf("performance score:%.2f%%",total);
+
     }
 }
