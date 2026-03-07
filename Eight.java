1a39015ee8abb8b82cb8b808c849e8f4be9981ca
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-07 19:39:47 +0530

    Add files via upload
    Hospital Patient Wait Time Optimizer

--- Eight.java
+++ Eight.java
@@ -1,18 +1,30 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Eight {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int org=n;
+        int[] arr=new int[n];
+        int max=Integer.MIN_VALUE;
         int sum=0;
-        while(n!=0){
-            int digit=n%10;
-            sum+=digit;
-            n=n/10;
+        int count=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]>60){
+                count++;
+            }
         }
-        System.out.println("Number: "+org);
-         System.out.println("sum of digits: "+sum);
-
+        double avg=(double)sum/n;
+        int total=n-count;
+        double num=((double)total/n)*100.0;
+        System.out.println("Total patients: "+n);
+        System.out.println("longest wait time: "+max+" minutes");
+        System.out.printf("Average wait time:%.2f Minutes\n",avg);
+        System.out.println("Patients waiting >60 min: "+count);
+        System.out.printf("Service level:%.2f%%",num);
     }
 }
