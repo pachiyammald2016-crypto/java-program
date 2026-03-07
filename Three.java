a6caedaccc02ad0e6d4c5bed68609ee1e0edfc62
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-07 19:37:08 +0530

    Add files via upload
    Student Grade Performance Analyzer

--- Three.java
+++ Three.java
@@ -1,28 +1,34 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Three {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=1;
+        int size=sc.nextInt();
+        int[] arr=new int[size];
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         int count=0;
-        while(m!=0){
-            m=sc.nextInt();
-            count++;
-            if(m>n){
-                System.out.println("Guess the number(1100):"+m);
-                System.out.println("Too High!");
+        int num=0;
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]>max){
+                max=arr[i];
             }
-            else if(m<n){
-                System.out.println("Guess the number(1100):"+m);
-                System.out.println("Too Low!");
+            if(arr[i]<40){
+                count++;
             }
-            else{
-                System.out.println("Guess the number(1100):"+m);
-                System.out.println("Correct! Attempts:"+count);
-            }
-
-
         }
+        for(int i=0;i<size;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        num=size-count;
+       double percent=((double)num/size)*100.0;
+        System.out.println("Total Students: "+size);
+        System.out.println("Highest Score: "+max);
+        System.out.println("lowest Score: "+min);
+        System.out.println("Failed Students: "+count);
+        System.out.printf("Pass percentage:%.2f%% ",percent);
     }
 }
