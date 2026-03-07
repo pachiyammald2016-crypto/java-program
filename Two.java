5f10ecddf3df13d7fa5c6ea586dc80c118112924
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-07 19:36:36 +0530

    Add files via upload
     Inventory Stock Level Tracker

--- Two.java
+++ Two.java
@@ -1,20 +1,31 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Two {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=1;
-        int sum=0;
+        int size=sc.nextInt();
+        int thresh=sc.nextInt();
+        int[] arr=new int[size];
         int count=0;
-        while(n!=0){
-            n=sc.nextInt();
-            sum+=n;
-            if(n>0){
-            count++;
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]<thresh){
+                count++;
             }
-            System.out.println("Enter number(0 to stop):"+n);
         }
-        System.out.println("Total sum: "+sum);
-        System.out.println("Count: "+count);
+        System.out.println("Total products: "+size);
+        System.out.println("minimum threshold: "+thresh);
+        System.out.println("low stock products: "+count);
+        System.out.print("product indices: ");
+        System.out.print("[");
+        for(int i=0;i<size;i++){
+            if(arr[i]<thresh){
+                System.out.print(i);
+                if(i<size-2){
+                System.out.print(", ");
+                }
+            }
+        }
+        System.out.print("]");
     }
 }
