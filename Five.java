7491b758f00adb80dfce13ea5f6069572960c2b5
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-07 19:38:01 +0530

    Add files via upload
     E-commerce Product Price Comparison Engine

--- Five.java
+++ Five.java
@@ -1,17 +1,27 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Five {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int org=n;
-        int rev=0;
-        while(n!=0){
-            int digit=n%10;
-            rev=rev*10+digit;
-            n=n/10;
+        int size=sc.nextInt();
+        double[] arr=new double[size];
+        double max=Double.MIN_VALUE;
+        double min=Double.MAX_VALUE;
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextDouble();
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
         }
-        System.out.println("Original: "+org);
-        System.out.println("Reversed: "+rev);
+        double num=max-min;
+        double diff=((double)num/max)*100;
+        System.out.println("Number of Sellers: "+size);
+        System.out.printf("Lowest Price:$%.2f\n",min);
+        System.out.printf("Highest Price:$%.2f\n",max);
+        System.out.printf("price difference:$%.2f\n",num);
+        System.out.printf("Savings:%.2f%%",diff);
     }
 }
