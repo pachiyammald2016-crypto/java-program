e5ac12f0e00f9598e94470f0db1c65a20f59ccde
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:47:27 +0530

    Add files via upload

--- Seventeen.java
+++ Seventeen.java
@@ -1,24 +1,45 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Seventeen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int org=n;
-        int count=n/2;
-        int i=1;
-        int sum=0;
-        while(i<=count){
-            if(n%i==0){
-                sum+=i;
+        double[] arr=new double[n];
+        double sum=0.0;
+        double max=Double.MIN_VALUE;
+        double min=Double.MAX_VALUE;
+        int count=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextDouble();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
             }
-            i++;
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>8.5&&arr[i]<6.5){
+                count++;
+            }
         }
-        if(sum==org){
-            System.out.println(org+" is a perfect number");
+        double avg=(double)sum/n;
+        System.out.println("total samples: "+n);
+        System.out.printf("avg pH: %.2f\n",avg);
+        System.out.printf("max pH: %.2f\n",max);
+        System.out.printf("min pH: %.2f\n",min);
+        System.out.println("unsafe samples: "+count);
+        int num=n-count;
+        double total=((double)num/n)*100;
+        System.out.printf("safety complience: %.2f%%\n",total);
+        System.out.print("Critical alerts: [");
+        for(int i=0;i<n;i++){
+            if(arr[i]>8.5&&arr[i]<6.5){
+                System.out.print(i);
+                if(i<n-2){
+                    System.out.print(", ");
+                }
+            }
         }
-        else{
-            System.out.println(org+" is not a perfect number");
-        }
+        System.out.print("]");
     }
 }
