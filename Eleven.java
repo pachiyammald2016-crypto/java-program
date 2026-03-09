d7b125f1984bff841746b5e1b01dbcac3ce1373c
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:45:18 +0530

    Add files via upload

--- Eleven.java
+++ Eleven.java
@@ -1,15 +1,36 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Eleven {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        while(b!=0){
-            int temp=b;
-            b=a%b;
-            a=temp;
+        int n=sc.nextInt();
+        double[] a=new double[n];
+        double[] b=new double[n];
+        double sum=0.0;
+        int index1=0;
+        int index2=0;
+        double max=Double.MIN_VALUE;
+        double min=Double.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextDouble();
+            for(int j=i;j<=i;j++){
+                b[j]=sc.nextDouble();
+                double s=a[i]*b[j];
+                sum+=s;
+                if(b[j]>max){
+                    max=b[j];
+                    index1=(int)j;
+                }
+                if(b[j]<min){
+                    min=b[j];
+                    index2=(int)j;
+                }
+            }
         }
-        System.out.println(a);
+        System.out.println("Number of assets: "+n);
+        System.out.printf("Total portfolio value:$%.2f",sum);
+        System.out.println("most valuable asset: asset "+index2);
+        System.out.println("least valuable asset: asset "+index1);
+        
     }
 }
