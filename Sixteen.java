bec224d4f2d3d54cf0f2674904d44e273fae9406
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:47:04 +0530

    Add files via upload

--- Sixteen.java
+++ Sixteen.java
@@ -1,20 +1,46 @@
-package While;
+package Array;
 import java.util.Scanner;
-import java.lang.Math;
 public class Sixteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int result=0;
-        int i=1;
-        int power=0;
-        while(n>0){
-            int digit=n%10;
-            power=digit*i;
-            result+=power;
-            n=n/10;
-            i+=i;
+        int[] arr=new int[n];
+        int sum=0;
+        int count=0;
+        int count1=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
+                count=i+1;
+            }
+
         }
-        System.out.println(result);
+        int avg=sum/n;
+        double s=(int)avg*1.5;
+        System.out.println("trading days: "+n);
+        System.out.println("total volume: "+sum);
+        System.out.println("avg daily volume: "+avg);
+        System.out.print("peak volume days: day "+count);
+        System.out.println("("+max+")");
+        for(int i=0;i<n;i++){
+            if(arr[i]>s){
+                count1++;
+            }
+        }
+        System.out.println("high volume days(>150% avg): "+count1);
+        System.out.print("Breakout signal days: [");
+        for(int i=0;i<n;i++){
+            if(arr[i]>s){
+                System.out.print(i);
+                if(i<n-2){
+                    System.out.print(", ");
+                }
+            }
+        }
+        System.out.print("]");
+        
     }
 }
