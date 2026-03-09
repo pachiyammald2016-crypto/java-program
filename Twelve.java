bf0374c774294038805e1450f8bb731ae2da669e
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:45:50 +0530

    Add files via upload

--- Twelve.java
+++ Twelve.java
@@ -1,21 +1,40 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Twelve {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int org=n;
-        int rev=0;
-        while(n!=0){
-            int digit=n%10;
-            rev=rev*10+digit;
-            n=n/10;
+        double[] arr=new double[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextDouble();
         }
-        if(rev==org){
-            System.out.println(org+" is palindrome");
+        double limit=sc.nextDouble();
+        int index=0;
+        double max=0.0;
+        double sum=0.0;
+        for(int j=0;j<n;j++){
+            sum+=arr[j];
+            if(arr[j]>max){
+                max=arr[j];
+                index=j;
+            }
+        }
+        String r="";
+        double avg=(double)sum/n;
+        System.out.println("Hours Monitored: "+n);
+        System.out.printf("total daily consumption:%.2f kwh\n",sum);
+        System.out.print("peak hour: hour "+index);
+        System.out.printf("( %.2f kwh)\n",max);
+        System.out.printf("average hourly:%.2f kwh\n",avg);
+        System.out.printf("Daily limit:%.2f kwh\n",limit);
+        double t=(double)sum-limit;
+        if(sum<limit){
+            r="Within limit";
+            System.out.println("Status: "+r);
         }
         else{
-            System.out.println(org+" is not palindrome");
+            r="Exceeded by";
+            System.out.printf("Exceeded by %.2f kwh",t);
         }
     }
 }
