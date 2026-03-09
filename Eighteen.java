fb44c09ae4ef626f00df7328e8bed011c9561e66
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:47:51 +0530

    Add files via upload

--- Eighteen.java
+++ Eighteen.java
@@ -1,22 +1,44 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Eighteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int m=a;
-        int r=b;
-        int gcd=0;
-        int lcm=0;
-        while(b!=0){
-            int temp=b;
-            b=a%b;
-            a=temp;
-            gcd=a;
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int sum=0;
+        int index=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
+                index=i+1;
+            }
         }
-        System.out.println(gcd);
-        lcm=m*r/gcd;
-        System.out.println(lcm);
+        double avg=(double)sum/n;
+        System.out.println("total products: "+n);
+        System.out.printf("avg monthly sales: %.2f\n",avg);
+        System.out.print("best-seller: product "+index);
+        System.out.println("("+max+" units)");
+        double num=avg*0.5;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<num){
+                count++;
+            }
+        }
+        System.out.println("slow-moving products: "+count);
+        System.out.printf("turnover threshold: %.2f units\n",num);
+        System.out.print("clearance candidate: [");
+        for(int i=0;i<n;i++){
+            if(arr[i]<num){
+                System.out.print(i);
+                if(i<n-2){
+                    System.out.print(", ");
+                }
+            }
+        }
+        System.out.print("]");
     }
 }
