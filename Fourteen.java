a3d0440c47760330cca6d2204620c9466458e051
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:46:20 +0530

    Add files via upload

--- Fourteen.java
+++ Fourteen.java
@@ -1,32 +1,36 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Fourteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int org=n;
-        int sum=0;
+        double[] arr=new double[n];
+        double max=Double.MIN_VALUE;
+        double sum=0.0;
+        int index=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextDouble();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
+                index=i;
+            }
+        }
+        double avg=(double)sum/n;
+        double total=avg*0.8;
+        System.out.println("Total plots: "+n);
+        System.out.printf("total harvest:%.2f tons\n",sum);
+        System.out.printf("average yield:%.2f tons/acre\n",avg);
+        System.out.printf("highest yield:%.2f tons/acre ",max);
+        System.out.println("(plot "+index+")");
         int count=0;
-        while(n!=0){
-            count++;
-            n=n/10;
+        for(int i=0;i<n;i++){
+            if(arr[i]<total){
+                count++;
+            }
         }
-        n=org;
-        while(n!=0){
-            int digit=n%10;
-            int fact=1;
-          for(int i=1;i<=count;i++){
-            fact*=digit;
-        }
-        sum+=fact;
-         n=n/10;
-    }
-    if(sum==org){
-        System.out.println(sum+" is amstrong number");
-    }
-    else{
-        System.out.println(sum+" is not amstrong number");
-    }
+        System.out.println("Underperforming plots: "+count);
+        System.out.printf("performance threshold:%.2f tons/acre",total);
 
     }
 }
