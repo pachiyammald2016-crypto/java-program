3b9ed12e917fd3b7d553a0c51ffc5a378f7deff7
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-09 06:46:41 +0530

    Add files via upload

--- Fifteen.java
+++ Fifteen.java
@@ -1,22 +1,35 @@
-package While;
+package Array;
 import java.util.Scanner;
 public class Fifteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int org=n;
+        int[] arr=new int[n];
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         int count=0;
-        do{
-            if(n%2==0){
-                n=n/2;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
             }
-            else{
-                n=3*n+1;
+            if(arr[i]<min){
+                min=arr[i];
             }
-           System.out.println(n);
-            count++;
+            if(arr[i]>300){
+                count++;
+            }
         }
-        while(n!=1);
-        System.out.println("steps: "+count);
+        double avg=(double)sum/n;
+        int total=n-count;
+        double s=((double)total/n)*100;
+        System.out.println("Total calls: "+n);
+        System.out.printf("Average Handling time:%.2f seconds\n",avg);
+        System.out.println("shortest call: "+min+" seconds");
+        System.out.println("longest call: "+max+" seconds");
+        System.out.println("Calls Exceeding target(300s): "+count);
+        System.out.printf("Target complience:%.2f%% ",s);
     }
 }
