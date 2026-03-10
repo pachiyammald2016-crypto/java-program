a53b7bce692fc43b90b7dc3ae159053f19f0ad81
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-10 06:44:56 +0530

    Add files via upload
    cybersecurity threat detection system

--- Twentefive.java
+++ Twentefive.java
@@ -1,16 +1,53 @@
+package Array;
 import java.util.Scanner;
 public class Twentefive {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int reg=sc.nextInt();
-        int broad=sc.nextInt();
-        int spon=sc.nextInt();
-        int prize=sc.nextInt();
-        int rent=sc.nextInt();
-        int ad=sc.nextInt();
-        int num1=reg+broad+spon-prize-rent-ad;
-        System.out.println(num1);
-
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int sum=0;
+        int event1=0;
+        int event2=0;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+            if(arr[i]>max){
+                max=arr[i];
+                event1=i;
+            }
+            if(arr[i]<min){
+                min=arr[i];
+                event2=i;
+            }
+        }
+        double avg=(double)sum/n;
+        System.out.println("Security events analyzed: "+n);
+        System.out.printf("Average risk score:%.2f \n",avg);
+        System.out.print("highest risk event: "+max);
+        System.out.println(" (event "+(event1+1)+")");
+        System.out.printf("lowest risk event: "+min);
+        System.out.println("(event "+(event2+1)+")");
+        double num=avg*2.0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>num){
+                count++;
+            }
+        }
+        System.out.printf("thread threshold:%.2f\n ",num);
+        System.out.println("critical threads(>200% avg): "+count);
+        System.out.print("Alert events: ");
+        System.out.print("[");
+        for(int i=0;i<n;i++){
+            if(arr[i]>num){
+                System.out.print(i+1);
+            if(i<n-2){
+                System.out.print(", ");
+            }
+        }
+        }
+        System.out.print("]");
     }
 }
-
