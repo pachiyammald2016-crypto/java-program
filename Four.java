a5d826fe5da55e5de05b9fe00d5c482881f42424
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-10 06:47:38 +0530

    Add files via upload
    sales performance dashboard

--- Four.java
+++ Four.java
@@ -1,29 +1,37 @@
-package Nestf;
+package Array2;
 import java.util.Scanner;
 public class Four {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int m=sc.nextInt();
-        double avg=0;
-        System.out.println("grade sheet:");
-        System.out.print("Students ");
-        for(int k=1;k<=m;k++){
-            System.out.print("sub:"+k+" ");
-        }
-        System.out.println("Average");
-        for(int i=1;i<=n;i++){
-            String name=sc.next();
-            System.out.print(name+" ");
-            int sum=0;
-            for(int j=1;j<=m;j++){
-                int mark=sc.nextInt();
-                sum+=mark;
-                System.out.print(mark+" ");
+        int[][] arr=new int[n][m];
+        System.out.println("Sales Data: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+                System.out.print(arr[i][j]+" ");
             }
-            avg=(double)sum/m;
-            System.out.print(avg+" ");
             System.out.println();
         }
+        int sum=0;
+        int max=0;
+        int index=0;
+        System.out.println();
+        System.out.println("product-wise total sales: ");
+        for(int i=0;i<n;i++){
+            sum=0;
+            System.out.print("product "+i+": ");
+            for(int j=0;j<m;j++){
+                sum+=arr[i][j];
+            }
+            System.out.print(sum);
+            System.out.println();
+            if(sum>max){
+                max=sum;
+                index=i;
+            }
+        }
+        System.out.println("product "+index+" with "+max+" units");
     }
 }
