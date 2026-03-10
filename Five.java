03eb5d75b000afafcc15147eebc7fe9cd42e0d56
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-10 06:48:55 +0530

    Add files via upload
    weather temperature grid analyzer

--- Five.java
+++ Five.java
@@ -1,27 +1,39 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Five {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        double[] arr=new double[size];
-        double max=Double.MIN_VALUE;
-        double min=Double.MAX_VALUE;
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextDouble();
-            if(arr[i]>max){
-                max=arr[i];
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] arr=new int[n][m];
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int index=0;
+        int index1=0;
+        int index2=0;
+        int index3=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+                sum+=arr[i][j];
+                if(arr[i][j]>max){
+                    max=arr[i][j];
+                    index=i;
+                    index1=j;
+                }
+                if(arr[i][j]<min){
+                    min=arr[i][j];
+                    index2=i;
+                    index3=j;
+                }
+                System.out.print(arr[i][j]+" ");
             }
-            if(arr[i]<min){
-                min=arr[i];
-            }
+            System.out.println();
         }
-        double num=max-min;
-        double diff=((double)num/max)*100;
-        System.out.println("Number of Sellers: "+size);
-        System.out.printf("Lowest Price:$%.2f\n",min);
-        System.out.printf("Highest Price:$%.2f\n",max);
-        System.out.printf("price difference:$%.2f\n",num);
-        System.out.printf("Savings:%.2f%%",diff);
+        int avg=sum/n*m;
+        System.out.println("maximum temp: "+max+" c at position ("+index+", "+index1+")");
+         System.out.println("maximum temp: "+min+" c at position ("+index2+", "+index3+")");
+        System.out.println("avg temp: "+avg+" c");
     }
 }
