c59c664b914da05fb3d595b00893f99b95116e51
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-10 06:46:58 +0530

    Add files via upload
    matrix addition calculator

--- Three.java
+++ Three.java
@@ -1,34 +1,52 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Three {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int[] arr=new int[size];
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        int count=0;
-        int num=0;
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
-            if(arr[i]>max){
-                max=arr[i];
-            }
-            if(arr[i]<40){
-                count++;
-            }
+   public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int m=sc.nextInt();
+    int[][] a=new int[n][m];
+    int[][] b=new int[n][m];
+    int[][] sum=new int[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            a[i][j]=sc.nextInt();
         }
-        for(int i=0;i<size;i++){
-            if(arr[i]<min){
-                min=arr[i];
-            }
-        }
-        num=size-count;
-       double percent=((double)num/size)*100.0;
-        System.out.println("Total Students: "+size);
-        System.out.println("Highest Score: "+max);
-        System.out.println("lowest Score: "+min);
-        System.out.println("Failed Students: "+count);
-        System.out.printf("Pass percentage:%.2f%% ",percent);
     }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            b[i][j]=sc.nextInt();
+        }
+    }
+    System.out.println("Matrix 1:" );
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            System.out.print(a[i][j]+" ");
+        }
+        System.out.println();
+    }
+    System.out.println();
+    System.out.println("Matrix 2:" );
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            System.out.print(b[i][j]+" ");
+        }
+        System.out.println();
+    }
+    System.out.println();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            sum[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    System.out.println("Sum Matrix: ");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            System.out.print(sum[i][j]+" ");
+        }
+        System.out.println();
+    }
+   } 
 }
+
+
