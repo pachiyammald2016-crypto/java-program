7800b630f344f456c9e96021a710da87a4e97d07
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-10 06:46:19 +0530

    Add files via upload
    cinema seat booking system

--- Two.java
+++ Two.java
@@ -1,31 +1,35 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Two {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int thresh=sc.nextInt();
-        int[] arr=new int[size];
-        int count=0;
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
-            if(arr[i]<thresh){
-                count++;
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int s=sc.nextInt();
+        char[][] arr=new char[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]='A';
             }
         }
-        System.out.println("Total products: "+size);
-        System.out.println("minimum threshold: "+thresh);
-        System.out.println("low stock products: "+count);
-        System.out.print("product indices: ");
-        System.out.print("[");
-        for(int i=0;i<size;i++){
-            if(arr[i]<thresh){
-                System.out.print(i);
-                if(i<size-2){
-                System.out.print(", ");
-                }
-            }
+        for(int k=0;k<s;k++){
+            int p=sc.nextInt();
+            int r=sc.nextInt();
+            arr[p][r]='B';
         }
-        System.out.print("]");
+        System.out.println("Seating chart: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        int num=n*m;
+        int total=num-s;
+        int rate=(s*10000)/num;
+        System.out.println("Total seats: "+num);
+        System.out.println("booked seats: "+s);
+        System.out.println("Available seats: "+total);
+        System.out.println("Occupancy rate: "+rate+"%");
     }
 }
