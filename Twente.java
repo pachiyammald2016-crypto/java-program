23d76398aa7cb04c8f69be6e18315fd42a96ceef
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-12 06:48:58 +0530

    Add files via upload
     Symmetric Matrix Checker

--- Twente.java
+++ Twente.java
@@ -1,46 +1,29 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Twente {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int[] arr=new int[n];
-        int sum=0;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        int index=0;
-        int index1=0;
+        int[][] arr=new int[n][n];
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-            sum+=arr[i];
-            if(arr[i]>max){
-                max=arr[i];
-                index=i+1;
-            }
-            if(arr[i]<min){
-                min=arr[i];
-                index1=i;
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
             }
         }
-        double avg=(double)sum/n;
-        double num=avg*0.6;
-        int index2=0;
+        boolean num=true;
         for(int i=0;i<n;i++){
-            if(arr[i]<60){
-                index2++;
+            for(int j=0;j<n;j++){
+                if(arr[i][j]!=arr[j][i]){
+                    num=false;
+                    break;
+                }
             }
         }
-        double total=n-index2;
-        double s=((double)total/n)*100;
-        System.out.println("total students: "+n);
-        System.out.printf("avg complience: %.2f%%\n",avg);
-        System.out.print("high com: "+max+"%");
-        System.out.println("(student "+index+")");
-         System.out.print("low com: "+min+"%");
-        System.out.println("(student "+index1+")");
-        System.out.println("at-risk students(<60%): "+index2);
-        System.out.printf("success rate: %.2f%%",s);
-
-
+        if(num){
+            System.out.println("Symmetric");
+        }
+        else{
+            System.out.println("Asymmetric");
+        }
     }
 }
