1c029740d47b83f810984aaf9aec59b0b08a7437
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-12 06:44:52 +0530

    Add files via upload
    Corporate Employee Contactbook

--- Fifteen.java
+++ Fifteen.java
@@ -1,35 +1,27 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Fifteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int[] arr=new int[n];
-        int sum=0;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        int count=0;
+        int total=0;
+        int[][] arr=new int[n][];
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-            sum+=arr[i];
-            if(arr[i]>max){
-                max=arr[i];
-            }
-            if(arr[i]<min){
-                min=arr[i];
-            }
-            if(arr[i]>300){
-                count++;
+                int size=sc.nextInt();
+                arr[i]=new int[size];
+                for(int j=0;j<size;j++){
+                    arr[i][j]=sc.nextInt();
+                    total++;
             }
         }
-        double avg=(double)sum/n;
-        int total=n-count;
-        double s=((double)total/n)*100;
-        System.out.println("Total calls: "+n);
-        System.out.printf("Average Handling time:%.2f seconds\n",avg);
-        System.out.println("shortest call: "+min+" seconds");
-        System.out.println("longest call: "+max+" seconds");
-        System.out.println("Calls Exceeding target(300s): "+count);
-        System.out.printf("Target complience:%.2f%% ",s);
+        System.out.println("Employee contact book: ");
+        for(int i=0;i<n;i++){
+            System.out.print("dept "+(i+1)+": ");
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("total employee: "+total);
     }
 }
