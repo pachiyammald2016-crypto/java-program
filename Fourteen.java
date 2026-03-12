3e43f459e67ea2f81ff13c6d7778f0219d5f4551
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-12 06:43:59 +0530

    Add files via upload
    Game Board State Rotation

--- Fourteen.java
+++ Fourteen.java
@@ -1,36 +1,24 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Fourteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        double[] arr=new double[n];
-        double max=Double.MIN_VALUE;
-        double sum=0.0;
-        int index=0;
+        int[][] arr=new int[n][n];
+        System.out.println("Original matrix: ");
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextDouble();
-            sum+=arr[i];
-            if(arr[i]>max){
-                max=arr[i];
-                index=i;
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+                System.out.print(arr[i][j]+" ");
             }
+            System.out.println();
         }
-        double avg=(double)sum/n;
-        double total=avg*0.8;
-        System.out.println("Total plots: "+n);
-        System.out.printf("total harvest:%.2f tons\n",sum);
-        System.out.printf("average yield:%.2f tons/acre\n",avg);
-        System.out.printf("highest yield:%.2f tons/acre ",max);
-        System.out.println("(plot "+index+")");
-        int count=0;
+        System.out.println("Rotated matrix (90 deg clockwise): ");
         for(int i=0;i<n;i++){
-            if(arr[i]<total){
-                count++;
+            for(int j=0;j<n;j++){
+                System.out.print(arr[j][n-1-i]+" ");
             }
+            System.out.println();
         }
-        System.out.println("Underperforming plots: "+count);
-        System.out.printf("performance threshold:%.2f tons/acre",total);
-
     }
 }
