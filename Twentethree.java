4c6482924c7635648cafbc081acf6dee8058e658
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-12 06:52:23 +0530

    Add files via upload
     Magic Square Validator

--- Twentethree.java
+++ Twentethree.java
@@ -1,42 +1,29 @@
-package Array;
+package Array2;
 import java.util.Scanner;
 public class Twentethree {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int[] arr=new int[n];
+        int[][] arr=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int num=0;
         int sum=0;
-        int max=Integer.MIN_VALUE;
-        int index=0;
         for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-            sum+=arr[i];
-            if(arr[i]>max){
-                max=arr[i];
-                index=i+1;
-            }
+            num+=arr[i][i];
         }
-        double num=(double)sum/n;
-        System.out.println("production lines: "+n);
-        System.out.printf("total output: "+sum+" units");
-        System.out.printf("average output:%.2f units/hour\n",num);
-        System.out.print("top producer: line "+index);
-        System.out.println("("+max+" units)");
-        double total=num*0.9;
-        System.out.printf("performance threshold:%.2f units\n",total);
-        int index2=0;
-        System.out.print("priority lines: [");
-        for(int i=0;i<n;i++){
-            if(arr[i]<total){
-                System.out.print(i+1);
-                if(i<n-2){
-                    System.out.print(", ");
-                }
-                index++;
-            }
+        for(int j=0;j<n;j++){
+            sum+=arr[j][n-j-1];
         }
-        System.out.print("]");
-
-
+        if(num==sum){
+            System.out.println("result: Magic square");
+        }
+        else{
+            System.out.println("result: not a magic square");
+        }
+        System.out.println("Constant: "+num);
     }
 }
