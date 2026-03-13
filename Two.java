ec5a2007cf04f4a3011f36e7919e7b5d89560ee9
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-13 06:53:39 +0530

    Add files via upload
    Password Strength Analyzer

--- Two.java
+++ Two.java
@@ -1,35 +1,40 @@
-package Array2;
+package String;
 import java.util.Scanner;
 public class Two {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
         int s=sc.nextInt();
-        char[][] arr=new char[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]='A';
+        sc.nextLine();
+        for(int i=0;i<s;i++){
+            String m=sc.nextLine(); 
+
+        boolean hasUpper=false;
+        boolean hasLower=false;
+        boolean hasDigit=false;
+        boolean hasSpecial=false;
+
+        for(int j=0;j<m.length();j++){
+            char ch=m.charAt(j);
+
+            if(Character.isUpperCase(ch)){
+                hasUpper=true;
+            }
+            else if(Character.isLowerCase(ch)){
+                hasLower=true;
+            }
+            else if(Character.isDigit(ch)){
+                hasDigit=true;
+            }
+            else{
+                hasSpecial=true;
             }
         }
-        for(int k=0;k<s;k++){
-            int p=sc.nextInt();
-            int r=sc.nextInt();
-            arr[p][r]='B';
+        if(m.length()>=8&&hasUpper&&hasLower&&hasDigit&&hasSpecial){
+            System.out.println(m+" -Strong");
         }
-        System.out.println("Seating chart: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
+        else{
+            System.out.println(m+" -Weak");
         }
-        int num=n*m;
-        int total=num-s;
-        int rate=(s*10000)/num;
-        System.out.println("Total seats: "+num);
-        System.out.println("booked seats: "+s);
-        System.out.println("Available seats: "+total);
-        System.out.println("Occupancy rate: "+rate+"%");
+      }
     }
 }
