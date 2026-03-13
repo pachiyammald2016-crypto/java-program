4be65cf8d9889aff02f6ff43dc1eb3bbfdd37292
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-13 06:54:44 +0530

    Add files via upload
    Text Message Compression

--- Three.java
+++ Three.java
@@ -1,52 +1,21 @@
-package Array2;
+package String;
 import java.util.Scanner;
 public class Three {
-   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int m=sc.nextInt();
-    int[][] a=new int[n][m];
-    int[][] b=new int[n][m];
-    int[][] sum=new int[n][m];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            a[i][j]=sc.nextInt();
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        System.out.println("Original: "+n);
+        int count=1;
+        for(int i=0;i<n.length();i++){
+            if(i<n.length()-1&&n.charAt(i)==n.charAt(i+1)){
+                count++;
+            }
+            else{
+                
+                char r=n.charAt(i);
+                System.out.print(r+""+count);
+                count=1;
+            }
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            b[i][j]=sc.nextInt();
-        }
-    }
-    System.out.println("Matrix 1:" );
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            System.out.print(a[i][j]+" ");
-        }
-        System.out.println();
-    }
-    System.out.println();
-    System.out.println("Matrix 2:" );
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            System.out.print(b[i][j]+" ");
-        }
-        System.out.println();
-    }
-    System.out.println();
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            sum[i][j]=a[i][j]+b[i][j];
-        }
-    }
-    System.out.println("Sum Matrix: ");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            System.out.print(sum[i][j]+" ");
-        }
-        System.out.println();
-    }
-   } 
 }
-
-
