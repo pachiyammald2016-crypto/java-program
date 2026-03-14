de30ad1087938e6cc2d8388faf73fdb357c4b0a7
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-14 09:31:20 +0530

    Add files via upload
     Genome Sequence DNA-to-RNA Transcriber

--- Thurteen.java
+++ Thurteen.java
@@ -1,51 +1,21 @@
-package Array2;
+package String;
 import java.util.Scanner;
 public class Thurteen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][] arr=new int[n][m];
+        sc.nextLine();
+        System.out.println("RNA Transcription: ");
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]=sc.nextInt();
+            String ch=sc.nextLine().toUpperCase();
+            if(ch.contains("T")){
+                String num=ch.replace('T','U');
+                System.out.println(ch+" -> "+num);
             }
-        }
-        System.out.println("Seating map: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==1){
-                    count++;
-                }
-            }
-        }
-        System.out.println("Total occupied: "+count);
-        int seats=n*m;
-        int rate=(count*10000)/seats;
-        System.out.println("Occupancy rate: "+rate+"%");
-        int sum=0;
-        int index=0;
-        System.out.print("Empty rows: [");
-        for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=0;j<m;j++){
-                sum+=arr[i][j];
-            }
-            if(sum==0){
-                index=i;
-                System.out.print(index);
-            if(i<n-2){
-                System.out.print(", ");
+            else{
+                System.out.println(ch+" -> "+ch);
             }
         }
         }
-        System.out.print("]");
     }
-}
+
