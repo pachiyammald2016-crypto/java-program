0114012496ea1304abf1fb036cd8b88b2d682618
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-14 09:32:48 +0530

    Add files via upload
     Log File Error Extractor

--- Sixteen.java
+++ Sixteen.java
@@ -1,46 +1,23 @@
-package Array;
+package String;
 import java.util.Scanner;
 public class Sixteen {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        int sum=0;
-        int count=0;
-        int count1=0;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-            sum+=arr[i];
-            if(arr[i]>max){
-                max=arr[i];
-                count=i+1;
-            }
-
+   public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    sc.nextLine();
+    int count=0;
+    System.out.println("Critical Logs: ");
+    for(int i=0;i<n;i++){
+        String s=sc.nextLine();
+        if(s.contains("ERROR")){
+            count++;
+            System.out.println(s);
         }
-        int avg=sum/n;
-        double s=(int)avg*1.5;
-        System.out.println("trading days: "+n);
-        System.out.println("total volume: "+sum);
-        System.out.println("avg daily volume: "+avg);
-        System.out.print("peak volume days: day "+count);
-        System.out.println("("+max+")");
-        for(int i=0;i<n;i++){
-            if(arr[i]>s){
-                count1++;
-            }
+        if(s.contains("EXCEPTION")){
+            count++;
+            System.out.println(s);
         }
-        System.out.println("high volume days(>150% avg): "+count1);
-        System.out.print("Breakout signal days: [");
-        for(int i=0;i<n;i++){
-            if(arr[i]>s){
-                System.out.print(i);
-                if(i<n-2){
-                    System.out.print(", ");
-                }
-            }
-        }
-        System.out.print("]");
-        
     }
+    System.out.println("total errors found: "+count);
+   } 
 }
