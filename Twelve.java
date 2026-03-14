beb77fdce7799c5b7b7616d64cc8c42b19d0d15b
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-03-14 09:30:43 +0530

    Add files via upload
     CSV Data Parser

--- Twelve.java
+++ Twelve.java
@@ -1,42 +1,19 @@
-package Array2;
+package String;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Twelve {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int[][] arr=new int[4][7];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<7;j++){
-                arr[i][j]=sc.nextInt();
-            }
-        }
-        System.out.println("Rainfall data: ");
-        for(int i=0;i<4;i++){
-            for(int j=0;j<7;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
+        int n=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Parsed fields: ");
+        for(int i=0;i<n;i++){
+            ArrayList<String> List=new ArrayList<>();
+            String num=sc.nextLine();
+            List.add(num);
+            int sum=i+1;
+            System.out.println(sum+". "+List);
 
-        int sum=0;
-        int count=0;
-        int index=0;
-        int zero=0;
-        System.out.println("Weekly totals: ");
-        for(int i=0;i<4;i++){
-            sum=0;
-            System.out.print("Week "+(i+1)+": ");
-            for(int j=0;j<7;j++){
-                sum+=arr[i][j];
-            }
-            if(sum==0){
-                zero=sum;
-                index=i+1;
-            }
-            System.out.print(sum+" mm");
-            System.out.println();
-            count+=sum;
         }
-        System.out.println("monthly total: "+count+" mm");
-        System.out.println("Driest week: week "+index+" ("+zero+" mm)");
     }
 }
