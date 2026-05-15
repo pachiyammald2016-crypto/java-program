b34770b6f80af3842423f0a8938042ab33e9fa60
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:04:14 +0530

    Add files via upload

--- Eleven.java
+++ Eleven.java
@@ -1,20 +1,16 @@
-package String;
-import java.util.Scanner;
+package Day1;
+
 public class Eleven {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.nextLine();
-        for(int i=0;i<n;i++){
-            String ch=sc.nextLine();
-            if(ch.contains("'")){
-                String num=ch.replace('\'','"');
-                System.out.println(ch+" -> "+num);
-            }
-            else{
-                System.out.println(ch+" -> "+ch);
-            }
-        }
+        int a=0;
+        int b=1;
+        int num=10;
+        for(int i=1;i<=num;i++){
+            int c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
         }
     }
     
+}
