433da01c3e3b4c9614bce9c1a6e724a7c2abc886
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:05:25 +0530

    Add files via upload

--- One.java
+++ One.java
@@ -1,31 +1,8 @@
-package String;
-import java.util.Scanner;
+package Day1;
+
 public class One {
-   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    sc.nextLine();
-
-    String num="^[A-Za-z0-9.+%-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-
-    int valid=0;
-    int invalid=0;
-    System.out.println("Email Validation Results: ");
-    for(int i=0;i<n;i++){
-      String p=sc.nextLine();
-
-      if(p.matches(num)){
-         int s=i+1;
-         System.out.println(s+". "+p+" - "+"Valid");
-         valid++;
-      }
-      else{
-         int s=i+1;
-         System.out.println(s+". "+p+" - "+"Invalid");
-         invalid++;
-      }
-    }
-    System.out.println("total valid: "+valid);
-    System.out.println("total invalid: "+invalid);
-   } 
+    public static void main(String[] args){
+        int num=10;
+        System.out.println(num);
+    }  
 }
