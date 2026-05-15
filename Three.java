6f82f5a858aaa797f7a46b921902feda0b9d8b3f
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:06:52 +0530

    Add files via upload

--- Three.java
+++ Three.java
@@ -1,21 +1,10 @@
-package String;
-import java.util.Scanner;
+package Day1;
+
 public class Three {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String n=sc.nextLine();
-        System.out.println("Original: "+n);
-        int count=1;
-        for(int i=0;i<n.length();i++){
-            if(i<n.length()-1&&n.charAt(i)==n.charAt(i+1)){
-                count++;
-            }
-            else{
-                
-                char r=n.charAt(i);
-                System.out.print(r+""+count);
-                count=1;
-            }
+        for(int i=10;i<=1;i--){
+            System.out.println(i);
         }
     }
+    
 }
