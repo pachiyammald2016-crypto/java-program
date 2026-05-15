742071f5c2bf06901dbae2a1d469d4202ca9038c
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:07:15 +0530

    Add files via upload

--- Twelve.java
+++ Twelve.java
@@ -1,19 +1,8 @@
-package String;
-import java.util.Scanner;
-import java.util.ArrayList;
+package Day1;
+
 public class Twelve {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.nextLine();
-        System.out.println("Parsed fields: ");
-        for(int i=0;i<n;i++){
-            ArrayList<String> List=new ArrayList<>();
-            String num=sc.nextLine();
-            List.add(num);
-            int sum=i+1;
-            System.out.println(sum+". "+List);
-
-        }
+        
     }
+    
 }
