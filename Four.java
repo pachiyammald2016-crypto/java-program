103f2461e72a22aa13758fde039c9200b1addcaf
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:04:48 +0530

    Add files via upload

--- Four.java
+++ Four.java
@@ -1,24 +1,15 @@
-package String;
-import java.util.Scanner;
+package Day1;
+
 public class Four {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.nextLine();
-        for(int i=0;i<n;i++){
-            String m=sc.nextLine();
-            String num="";
-            int r=m.length();
-
-            for(int j=r-1;j>=0;j--){
-                 num+=m.charAt(j);   
+        int sum=0;
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                sum+=i;
             }
-            if(num.equalsIgnoreCase(m)){
-            System.out.println(m+" - Palindrome");
         }
-        else{
-            System.out.println(m+" - Not Palindrome");
-        }
-        }
+        System.out.println(sum);
+        
     }
+    
 }
