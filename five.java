02e79713c608e8768ecd94761dc13987d96e914a
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:04:31 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class five {
+    public static void main(String[] args){
+        int num=2;
+        int mul=1;
+        for(int i=1;i<=10;i++){
+            mul*=i;
+            System.out.println(i + "*" + num + "="+ mul);
+        }
+    }
+    
+}
