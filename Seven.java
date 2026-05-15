e3a2a1d199d592d74c6fb98eeff7f338f6e6400a
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:05:45 +0530

    Add files via upload

--- Seven.java
+++ Seven.java
@@ -1,18 +1,15 @@
-package String;
-import java.util.Scanner;
+package Day1;
+
 public class Seven {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.nextLine();
-        for(int i=0;i<n;i++){
-            String s=sc.nextLine();
-            String[] num=s.split("-");
-            String airline=num[0].substring(0,2);
-            String flight=num[0].substring(2);
-            String from=num[1];
-            String to=num[2];
-            System.out.println("Airline: "+airline+", Flight: "+flight+", From: "+from+", To: "+to);                                                                       
+        int num=121;
+        int rev=0;
+        while(num!=0){
+            int digit=num%10;
+            rev=rev*10+digit;
+            num=num/10;
         }
+        System.out.println(rev);
     }
+    
 }
