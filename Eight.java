c4dad73287613bd539ea551dfb3e46e22f598cdc
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:03:56 +0530

    Add files via upload

--- Eight.java
+++ Eight.java
@@ -1,33 +1,20 @@
-package Array2;
-import java.util.Scanner;
+package Day1;
+
 public class Eight {
-   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int m=sc.nextInt();
-    int s=sc.nextInt();
-    int[][] arr=new int[n][m];
-    System.out.println("original image:");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            arr[i][j]=sc.nextInt();
-            System.out.print(arr[i][j]+" ");
+    public static void main(String[] args){
+        int num=123;
+        int sum=0;
+        while(num!=0){
+            int digit=num%10;
+            int rev=sum+(digit*digit*digit);
+            num=num/10;
         }
-        System.out.println();
-    }
-    System.out.println();
-    System.out.println("Adjusted image: ");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(arr[i][j]<255){
-                arr[i][j]=arr[i][j]+s;
-            }
-            if(arr[i][j]>255){
-                arr[i][j]=255;
-            }
-            System.out.print(arr[i][j]+" ");
+        if(sum==num){
+            System.out.println("it is true");
         }
-        System.out.println();
+        else{
+            System.out.println("it is false");
+        }
     }
-   } 
+    
 }
