0e43bbcb5b945cea9a9b8433cb1b4cbbde5c7408
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:06:27 +0530

    Add files via upload

--- Ten.java
+++ Ten.java
@@ -1,40 +1,15 @@
-package Array2;
-import java.util.Scanner;
+package Day1;
+
 public class Ten {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][] arr=new int[n][m];
-        int sum=0;
-        System.out.println("sales matrix: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]=sc.nextInt();
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
+        int num=123;
+        int fact=1;
+        while(num!=0){
+            int digit=num%10;
+            fact=fact*digit;
+            num=num/10;
         }
-        System.out.println();
-        System.out.println("representative totals: ");
-        for(int i=0;i<n;i++){
-            sum=0;
-            System.out.print("rep "+i+": ");
-            for(int j=0;j<m;j++){
-                sum+=arr[i][j];
-            }
-            System.out.println(sum);
-        }
-        System.out.println();
-        System.out.println("terrinary totals: ");
-        int sum2=0;
-        for(int i=0;i<m;i++){
-            sum2=0;
-            System.out.print("terrinary "+i+": ");
-            for(int j=0;j<n;j++){
-                sum2+=arr[j][i];
-            }
-            System.out.println(sum2);
-        }
+        System.out.println(fact);
     }
+    
 }
