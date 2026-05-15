616284953ecd01a7d60d7e69062729e40e6211c4
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-15 21:05:06 +0530

    Add files via upload

--- Nine.java
+++ Nine.java
@@ -1,32 +1,16 @@
-package Array2;
-import java.util.Scanner;
+package Day1;
+
 public class Nine {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][] arr=new int[n][m];
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        System.out.println("Elevation map:");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]=sc.nextInt();
-                System.out.print(arr[i][j]+" ");
-                if(arr[i][j]>max){
-                    max=arr[i][j];
-                }
-                if(arr[i][j]<min){
-                    min=arr[i][j];
-                }
-            }
-            System.out.println();
+        int num=1234;
+        int sum=0;
+        while(num!=0){
+            int digit=num%10;
+            sum=sum+digit;
+            num=num/10;
         }
-        System.out.println();
-        int range=max-min;
-        System.out.println("highest point: "+max+" m");
-        System.out.println("lowest point: "+min+" m");
-        System.out.println("elevation range: "+range+" m");
-
+        System.out.println(sum);
     }
+
+    
 }
