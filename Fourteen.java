688d68d152f2b8a93bd863bd18e7ab0fffc2decc
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-16 21:34:55 +0530

    Add files via upload

--- Fourteen.java
+++ Fourteen.java
@@ -1,24 +1,22 @@
-package Array2;
-import java.util.Scanner;
+package Day1;
+
 public class Fourteen {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[][] arr=new int[n][n];
-        System.out.println("Original matrix: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j]=sc.nextInt();
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println("Rotated matrix (90 deg clockwise): ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(arr[j][n-1-i]+" ");
-            }
-            System.out.println();
-        }
+    static void swap(int a , int b){
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping: ");
+        System.out.println("a is :"+a);
+        System.out.println("b is: "+b);
+
+
     }
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        swap(a,b);
+        
+    }
+    
 }
