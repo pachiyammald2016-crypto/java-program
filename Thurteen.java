deef2b4ca729a21b6b70cc31fba71257972a8e19
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-16 21:34:31 +0530

    Add files via upload

--- Thurteen.java
+++ Thurteen.java
@@ -1,21 +1,14 @@
-package String;
-import java.util.Scanner;
-public class Thurteen {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.nextLine();
-        System.out.println("RNA Transcription: ");
-        for(int i=0;i<n;i++){
-            String ch=sc.nextLine().toUpperCase();
-            if(ch.contains("T")){
-                String num=ch.replace('T','U');
-                System.out.println(ch+" -> "+num);
-            }
-            else{
-                System.out.println(ch+" -> "+ch);
-            }
-        }
-        }
-    }
+package Day1;
 
+public class Thurteen {
+    static int sum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int num=sum(a,b);
+        System.out.println(num);
+    }
+    
+}
