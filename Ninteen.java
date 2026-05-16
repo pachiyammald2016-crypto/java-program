931f72851e4b8f1c503ba1afb037186561784cce
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-16 21:37:44 +0530

    Add files via upload

--- Ninteen.java
+++ Ninteen.java
@@ -1,33 +1,25 @@
-package Array2;
-import java.util.Scanner;
+package Day1;
+
 public class Ninteen {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[][] arr=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j]=sc.nextInt();
-            }
+    static void digit(int a){
+        int sum=0;
+        int fact=1;
+        while(a!=0){
+            int num=a%10;
+            sum+=num;
+            fact*=num;
+            a=a/10;
         }
-        System.out.println("original image: ");
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
+        if(sum==fact){
+            System.out.println("it is spy number");
         }
-        int[][] a=new int[n/2][n/2];
-        for(int i=0;i<n;i+=2){
-            for(int j=0;j<n;j+=2){
-                a[i/2][j/2]=arr[i][j];
-            }
-        }
-        for(int i=0;i<n/2;i++){
-            for(int j=0;j<n/2;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
+        else{
+            System.out.println("it is not spy number");
         }
     }
+    public static void main(String[] args){
+        int a=123;
+        digit(a);
+    }
+    
 }
