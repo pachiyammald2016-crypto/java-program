ddc90eaa0c4b1652b70b3837c8e8cec2f35d7fa7
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-16 21:36:52 +0530

    Add files via upload

--- Eighteen.java
+++ Eighteen.java
@@ -1,21 +1,17 @@
-package String;
-import java.util.Scanner;
+package Day1;
+
 public class Eighteen {
-   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    String s=sc.nextLine();
-    String num="";
-    for(int i=0;i<s.length();i++){
-      char ch=s.charAt(i);
-      if(Character.isLowerCase(ch)){
-         ch=(char)((ch-'a'+3)%26+'a');
-      }
-      if(Character.isUpperCase(ch)){
-         ch=(char)((ch-'A'+3)%26+'A');
-      }
-      num+=ch;
+    static int digit(int a){
+        while(a!=0){
+            int num=a%10;
+            System.out.println(num);
+            a=a/10;
+        }
+        return 0;
     }
-    System.out.println("Original: "+s);
-    System.out.println("Encrypted: "+num);
-   } 
+    public static void main(String[] args){
+        int a=1234;
+        digit(a);
+    }
+    
 }
