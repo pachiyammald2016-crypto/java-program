8fdb41dd5995ded716fdb77e03caf1d26ba0cfc2
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-18 22:28:41 +0530

    Add files via upload

--- Four.java
+++ Four.java
@@ -1,15 +1,7 @@
-package Day1;
+package Day2;
 
-public class Four {
+public class Four{
     public static void main(String[] args){
-        int sum=0;
-        for(int i=1;i<=100;i++){
-            if(i%2==0){
-                sum+=i;
-            }
-        }
-        System.out.println(sum);
-        
+        System.out.printf("Happy Learning!");
     }
-    
 }
