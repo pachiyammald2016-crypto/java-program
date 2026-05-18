6878d934a2fc2493c95d75f63eb2ac62cacbdef5
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-18 22:29:07 +0530

    Add files via upload

--- Five.java
+++ Five.java
@@ -1,39 +1,11 @@
-package Array2;
+package Day2;
+
 import java.util.Scanner;
-public class Five {
+public class Five{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][] arr=new int[n][m];
-        int sum = 0;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        int index=0;
-        int index1=0;
-        int index2=0;
-        int index3=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]=sc.nextInt();
-                sum+=arr[i][j];
-                if(arr[i][j]>max){
-                    max=arr[i][j];
-                    index=i;
-                    index1=j;
-                }
-                if(arr[i][j]<min){
-                    min=arr[i][j];
-                    index2=i;
-                    index3=j;
-                }
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-        int avg=sum/n*m;
-        System.out.println("maximum temp: "+max+" c at position ("+index+", "+index1+")");
-         System.out.println("maximum temp: "+min+" c at position ("+index2+", "+index3+")");
-        System.out.println("avg temp: "+avg+" c");
+        long num1=sc.nextLong();
+        long num2=sc.nextLong();
+        System.out.printf("%d",num1+num2);
     }
 }
