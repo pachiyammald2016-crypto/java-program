e8df4c7575a3051ab2890d3bdbe0e8e4925cb3f4
Pachiyammal Durai <pachiyammald2016@gmail.com>
2026-05-18 22:28:00 +0530

    Add files via upload

--- Two.java
+++ Two.java
@@ -1,40 +1,10 @@
-package String;
+package Day2;
+
 import java.util.Scanner;
-public class Two {
+public class Two{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int s=sc.nextInt();
-        sc.nextLine();
-        for(int i=0;i<s;i++){
-            String m=sc.nextLine(); 
-
-        boolean hasUpper=false;
-        boolean hasLower=false;
-        boolean hasDigit=false;
-        boolean hasSpecial=false;
-
-        for(int j=0;j<m.length();j++){
-            char ch=m.charAt(j);
-
-            if(Character.isUpperCase(ch)){
-                hasUpper=true;
-            }
-            else if(Character.isLowerCase(ch)){
-                hasLower=true;
-            }
-            else if(Character.isDigit(ch)){
-                hasDigit=true;
-            }
-            else{
-                hasSpecial=true;
-            }
-        }
-        if(m.length()>=8&&hasUpper&&hasLower&&hasDigit&&hasSpecial){
-            System.out.println(m+" -Strong");
-        }
-        else{
-            System.out.println(m+" -Weak");
-        }
-      }
+        char num=sc.next().charAt(0);
+        System.out.printf("%c",num);
     }
 }
